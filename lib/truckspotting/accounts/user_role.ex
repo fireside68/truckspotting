a489470f4c5b857d338ad83b5701716650356d75
Cedric Johnson <cjohns0913@gmail.com>
2023-08-14 21:17:08 -0600

    Add user validation

--- lib/truckspotting/accounts/user_role.ex
+++ lib/truckspotting/accounts/user_role.ex
@@ -11,7 +11,6 @@ defmodule Truckspotting.Accounts.UserRole do
   import Ecto.Changeset
 
   schema "user_roles" do
-
     field :user_id, :id
     field :role_id, :id
 
@@ -21,7 +20,17 @@ defmodule Truckspotting.Accounts.UserRole do
   @doc false
   def changeset(user_role, attrs) do
     user_role
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:user_id, :role_id])
+    |> validate_required([:user_id, :role_id])
+    |> validate_is_user(:user_id)
+  end
+
+  def validate_is_user(changeset, field) when is_atom(field) do
+    validate_change(changeset, field, fn field, value ->
+      case Truckspotting.Accounts.get_user(value) do
+        %Truckspotting.Accounts.User{} = _ -> []
+        _ -> [{field, "This references a nonexistent user"}]
+      end
+    end)
   end
 end
