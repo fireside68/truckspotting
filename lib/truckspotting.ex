51f2af8d8866692bd74879c83bd30f07ea8e6f56
Cedric Johnson <cjohns0913@gmail.com>
2023-08-15 23:03:49 -0600

    Build context macro

--- lib/truckspotting.ex
+++ lib/truckspotting.ex
@@ -6,4 +6,18 @@ defmodule Truckspotting do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def context do
+    quote do
+      import Ecto.Query, warn: false
+      alias Truckspotting.Repo
+    end
+  end
+
+  @doc """
+  When used, dispatch to the appropriate controller/view/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
